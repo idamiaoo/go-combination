ab440d58009437b822f6830838c8e0c398daef2b
longzl <610960686@qq.com>
2016-11-03 17:20:29 +0800

    dd

--- combination.go
+++ combination.go
@@ -1,48 +1,46 @@
-package conmbinnation
+package combinnations
 
-//SampleIndexs 获取所有组合的元素索引
-func SampleIndexs(total, n int) [][]int {
-	var result [][]int
-	indexs := make([]int, total)
-	for i := 0; i < n; i++ {
-		indexs[i] = 1
-	}
-
-	tmpindex := make([]int, total)
-	copy(tmpindex, indexs)
-	result = append(result, tmpindex)
-	for {
-		find := false
-		for i := 0; i < total-1; i++ {
-			if indexs[i]-indexs[i+1] == 1 {
-				indexs[i], indexs[i+1] = 0, 1
-				moveOneToleft(indexs[:i])
-				tmpindex := make([]int, total)
-				copy(tmpindex, indexs)
-				result = append(result, tmpindex)
-				find = true
-				break
-			}
-		}
-		if !find {
-			break
-		}
-	}
-	return result
+type combinnations struct {
+	n    int
+	k    int
+	comb []int
 }
 
-func moveOneToleft(leftindexs []int) {
-	sum := 0
-	for _, v := range leftindexs {
-		if v == 1 {
-			sum++
-		}
+//NewConmbinnations ,,,
+func NewConmbinnations() *combinnations {
+	return &combinnations{}
+}
+
+func (c *combinnations) Init(n, k int) bool {
+	if k > n || k <= 0 {
+		return false
 	}
-	for i := range leftindexs {
-		if i < sum {
-			leftindexs[i] = 1
-		} else {
-			leftindexs[i] = 0
-		}
+	c.n = n
+	c.k = k
+	c.comb = make([]int, k)
+	for i := 0; i < k; i++ {
+		c.comb[i] = i
 	}
+	return true
+}
+
+func (c *combinnations) Next() bool {
+	ii := c.k - 1
+	for ii >= 0 && c.comb[ii]+c.k+1 > c.n+ii {
+		ii--
+	}
+	if ii < 0 {
+		return false
+	}
+	c.comb[ii]++
+	ii++
+	for ii < c.k {
+		c.comb[ii] = c.comb[ii-1] + 1
+		ii++
+	}
+	return true
+}
+
+func (c *combinnations) Comb() []int {
+	return c.comb
 }
